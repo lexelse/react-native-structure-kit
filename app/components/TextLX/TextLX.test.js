672ec7d294f45d34129ac5f74a9114a3525804c7
asimolmez <asimolmez@outlook.com>
2019-07-14 11:53:00 +0300

    enzyme integretad

---
+++ app/components/TextLX/TextLX.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {TextLX} from './TextLX';
+
+describe('TextLX', () => {
+    describe('Rendering', () => {
+        it('should match to snapshot', () => {
+            const component = shallow(<TextLX />)
+            expect(component).toMatchSnapshot()
+        });
+    });
+});
